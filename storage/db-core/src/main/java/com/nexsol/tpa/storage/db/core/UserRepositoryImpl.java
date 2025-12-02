f4b264fdcb9ced5c0b098284a15b1f3912d06d1d
devback90 <devback90.nexsol@gmail.com>
2025-12-02 15:30:06 +0900

    feat: 데이터 엑세스 레이어

---
+++ storage/db-core/src/main/java/com/nexsol/tpa/storage/db/core/UserRepositoryImpl.java
@@ -0,0 +1,43 @@
+package com.nexsol.tpa.storage.db.core;
+
+import com.nexsol.tpa.core.domain.User;
+import com.nexsol.tpa.core.domain.UserRepository;
+import com.nexsol.tpa.core.error.CoreErrorType;
+import com.nexsol.tpa.core.error.CoreException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class UserRepositoryImpl implements UserRepository {
+    private final UserJpaRepository userJpaRepository;
+
+    @Override
+    public Optional<User> findByCompanyCodeAndEmail(String companyCode, String email) {
+        return userJpaRepository.findByCompanyCodeAndEmail(companyCode, email)
+                .map(UserEntity::toDomain);
+    }
+
+    @Override
+    public boolean existsCompanyCodeAndEmail(String companyCode, String email) {
+        return userJpaRepository.existsByCompanyCodeAndEmail(companyCode, email);
+    }
+
+    @Override
+    public User save(User user) {
+        if (user.id() == null) {
+
+            return userJpaRepository.save(UserEntity.fromDomain(user)).toDomain();
+        } else {
+
+            UserEntity entity = userJpaRepository.findById(user.id())
+                    .orElseThrow(() -> new CoreException(CoreErrorType.USER_NOT_FOUND));
+
+            entity.update(user);
+
+            return userJpaRepository.save(entity).toDomain();
+        }
+    }
+}
