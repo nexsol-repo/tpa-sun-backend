92b4daae398860ecbc9e6aedec54f6cdcc95a332
devback90 <devback90.nexsol@gmail.com>
2025-12-12 12:02:00 +0900

    fix: db 컬럼네이밍 수정

--- storage/db-core/src/main/java/com/nexsol/tpa/storage/db/core/InsurancePlantEmbeddable.java
+++ storage/db-core/src/main/java/com/nexsol/tpa/storage/db/core/InsurancePlantEmbeddable.java
@@ -1,6 +1,7 @@
 package com.nexsol.tpa.storage.db.core;
 
 import com.nexsol.tpa.core.domain.InsurancePlant;
+import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import lombok.NoArgsConstructor;
 
@@ -13,20 +14,28 @@ public class InsurancePlantEmbeddable {
 
 	private String plantName;
 
+	@Column(name = "plant_address")
 	private String address;
 
+	@Column(name = "plant_region")
 	private String region;
 
+	@Column(name = "plant_capacity")
 	private BigDecimal capacity;
 
+	@Column(name = "plant_area")
 	private BigDecimal area;
 
+	@Column(name = "plant_inspection_date")
 	private LocalDate inspectionDate;
 
+	@Column(name = "plant_facility_type")
 	private String facilityType;
 
+	@Column(name = "plant_drive_method")
 	private String driveMethod;
 
+	@Column(name = "plant_sales_target")
 	private String salesTarget;
 
 	public InsurancePlantEmbeddable(InsurancePlant domain) {
